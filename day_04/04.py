dba4d59a764a90c7bb7fc930fdae3d3cb39cd760
Kimi Nikolov <bytebuilder220@gmail.com>
2023-12-13 21:41:24 +0200

    Finished part 2ß

--- day_04/04.py
+++ day_04/04.py
@@ -1,28 +1,17 @@
 with open('day_04/04_input.txt') as d:
     lines = d.readlines()
+from collections import defaultdict
 
-total_value = 0
+N = defaultdict(int)
 
-def get_value(line: str) -> int:
-    line_value = 0
-    card_info = line.split(": ")
-    _, card_id = card_info[0].split()
-    my_numbers = card_info[1].split(" | ")[0].split()
-    winning_numbers = card_info[1].split(" | ")[1].split()
+for i, line in enumerate(lines):
+    N[i] += 1
+    first, rest = line.split("|")
+    id_, card = first.split(":")
+    card_nums = [int(x) for x in card.split()]
+    rest_nums = [int(x) for x in rest.split()]
+    val = len(set(card_nums) & set(rest_nums))
+    for j in range(val):
+        N[i+1+j] += N[i]
 
-    my_numbers = [int(x) for x in my_numbers]
-    winning_numbers = [int(x) for x in winning_numbers]
-
-    for number in my_numbers:
-        if number in winning_numbers:
-            if line_value == 0:
-                line_value = 1
-            else: 
-                line_value *= 2
-    
-    return line_value
-
-for i in range(0, len(lines)):
-    total_value += get_value(lines[i])
-
-print(total_value)
+print(sum(N.values()))
